d726abb1eeb5448c6f9d17cdad7ba2c1eabc6b55
stevitech <81750989+stevitech@users.noreply.github.com>
2022-09-16 01:31:55 +0200

    added more routes

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const app = express();
+app.all("/", (req, res) => {
+    console.log("Just got a request!");
+    res.send("Yo!");
+});
+
+app.get("/api", (req, res) => {
+    const path = `/api/item`;
+    res.setHeader("Content-Type", "text/html");
+    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.listen(process.env.PORT || 3000);
